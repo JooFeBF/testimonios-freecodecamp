27157a2cd1be37fd877181adac4fc86db2c68b26
Joseph Martinez <insertepala@gmail.com>
2024-06-25 09:32:28 -0500

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+
+  return (
+    <>
+      <div className='App'>
+        <div className='contenedor-principal'>
+          <h1>Esto es lo que dicen nuestros alumnos sobre  freeCodeCamp:</h1>
+
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
